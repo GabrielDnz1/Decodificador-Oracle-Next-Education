d86414033f1b7fbbb45111bb662b2257a8c569ef
GabrielDnz1 <gabrielbarbosa1315@gmail.com>
2024-01-24 13:21:34 -0300

    Finalização do projeto AluraONE

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let imagemDoContainer = document.getElementById('imagem_retangulo');
+let imagemDoContainer = document.getElementById('imagem_do_container_lateral');
 let tituloDoContainer = document.getElementById('titulo_retangulo');
 let subtituloDoContainer = document.getElementById('subtitulo_retangulo');
 let textoDoInput = document.getElementById('inputDeTexto');
@@ -8,12 +8,47 @@ var botaoCodificador = document.querySelector("botao_criptografar");
 var botaoDescodificador = document.querySelector("botao_descriptografar");
 
 function codificar() {
+    //MUDANÇAS NA INTERFACE
     botaoDeCopiar.style.display = "flex";
     imagemDoContainer.remove();
     subtituloDoContainer.remove();
     tituloDoContainer.remove();
-    textoDaResposta.textContent = textoDoInput.value;
+
+    //CRIPTOGRAFIA
+
+    let text = document.getElementById("inputDeTexto").value;
+
+    text = text.replace(/e/g, "enter");
+    text = text.replace(/i/g, "imes");
+    text = text.replace(/a/g, "ai");
+    text = text.replace(/o/g, "ober");
+    text = text.replace(/u/g, "ufat");
+
+    console.log('o valor tá', {text} );
+    document.getElementById("texto_resposta").textContent = text;
 }
+
+function decodificar() {
+    //MUDANÇAS NA INTERFACE
+    botaoDeCopiar.style.display = "flex";
+    imagemDoContainer.remove();
+    subtituloDoContainer.remove();
+    tituloDoContainer.remove();
+
+    //CRIPTOGRAFIA
+
+    let text = document.getElementById("inputDeTexto").value;
+
+    text = text.replace(/enter/g, 'e');
+    text = text.replace(/imes/g, 'i');
+    text = text.replace(/ai/g, 'a');
+    text = text.replace(/ober/g, 'o');
+    text = text.replace(/ufat/g, 'u');
+
+    console.log('o valor tá', {text} );
+    document.getElementById("texto_resposta").textContent = text;
+}
+
 
 function copiarValorDoInput() {
     var textoParaCopiar = textoDaResposta.textContent;
